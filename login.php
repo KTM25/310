a695c06f6fdf5bad229c71811a0e351a2c14fe9a
KTM25 <ktmangus26@gmail.com>
2017-02-17 17:09:24 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,91 @@
+<?php
+session_start(); $username = $password = $userError = $passError = '';
+if(isset($_POST['sub'])){
+  $username = $_POST['username']; 
+  $password = $_POST['password'];
+  // $date = $_POST['m/d/Y h:i'];
+  
+  if(($username === 'admin' && $password === 'password') || ($username === 'ktmangus' && $password === 'ktmangus25')){
+    $_SESSION['login'] = true; 
+    header('LOCATION:login.php'); 
+    die();
+  }
+  if($username !== 'admin'){
+    $userError = 'Invalid Username';
+    // $date;
+  }
+
+  if($password !== 'password'){
+    $passError = 'Invalid Password ';
+    date('m/d/Y');
+  }
+}
+?>
+
+
 <!Doctype html>
 <html>
 <head>
-    <meta name="Author" content="Kevin Mangus" name="Description" content="Kevin M. Login Form" name="keywords" content="HTML, CSS, XML">
+    <meta name='Author' content='Kevin Mangus' name='Description' content='Kevin M. Login Form' name='keywords' content='HTML, CSS, XML'>
     <title>Login Page</title>
-    <link rel="stylesheet" href="./style.css" type="text/css" />
+    <link rel='stylesheet' href='./style.css' type='text/css' />
 </head>
 
 <body>
+  <div class='header'><h1>Login Form Page</h1> </div>
+    <div class='wrapper'>
+        
 
-    <div class="wrapper">
-        <div class="header"><h1>Login Form Page</h1> </div>
+        <nav class="navigation">
+            <ul>
+               <li><a class="active" href="../index.html">Home</a></li>
+            </ul>
+        </nav>
+        
+        <form name='input' action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
 
+        <label for='username'></label>
+        <input type='text' value='<?php echo $username; ?>' id='username' name='username' placeholder='Enter Password' />
 
-        <form id="login" action="login.php">
+        <div class='error'>$userError</div>
+
+        <label for='password'></label>
+        <input type='password' value='<?php echo $password; ?>' id='password' name='password' placeholder='Enter Password' />
+
+        <div class='error'>
+          <?php echo $passError; ?>
+        </div>
+        <input type='submit' value='loginbtn' name='sub' />
+  </form>
+        <!--<form name='input' class="form"  action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+          <div class="container" >
+
+            <br/>
+            <label for='username'></label>
             
-            <div class="container">
-                
-                <label><b>Username</b></label>
+            <input type='text' value='<?php echo $username; ?>' placeholder='Enter Username' id=username name='username' required>
 
-                <input type="text" placeholder="Enter Username" name="uname" required> <!--creates the username box end of required to make sure its inputed-->
-
-                <label><b>Password</b></label>
-
-                <input type="password" placeholder="Enter Password" name="psw" required>
-
-                <button type="Submit" class="loginbtn">Login</button>
-
-            </div><!-- end of class container -->
-
-            <div class="container" style="background-color: #f1f1f1">
-                <button type="cancel" class="cancelbtn" >Cancel</button>
+            <div class='error'>
+              <?php echo $userError; ?>
             </div>
 
-        </form>
+            <label for='password'></label>
+            <input type='password' value='<?php echo $password; ?>' placeholder="Enter Password" id='password' name='password' required/>
 
-        <div class="footer">
+            <button type='Submit' class='loginbtn'>Login</button>
+
+            <div class='error'>
+              <?php echo  $passError; ?> 
+            </div>
+            <br/>
+
+            <div class='container' style='background-color: #f1f1f1'>
+                <button type='cancel' class='cancelbtn' >Cancel</button>
+            </div>
+          </div>
+        </form>-->
+
+        <div class='footer'>
             &copy; Copyright 2017, <!-- &copy get the copyright symbol -->
             Kevin Mangus
         </div>
@@ -43,4 +94,3 @@
 
 </body>
 </html>
-
