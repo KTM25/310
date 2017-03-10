b5ca88ee73671159d59a081a80de72cfc7e4947e
KTM25 <ktmangus26@gmail.com>
2017-03-09 22:50:25 -0700

    fixed stuff

--- Project_1/header.php
+++ Project_1/header.php
@@ -2,12 +2,6 @@
 <html>
 <body>
 <div class="jumbotron">
-<<<<<<< HEAD
-      
-    <img src="../rams.png" class="img-circle hidden-xs" id="Rams" alt="Rams Head" height="126" width="126">
-    
-     <h1 id="page_title" style='font-size: 50px; width: auto;'> Ingredients! </h1>
-=======
 
    
 
@@ -21,14 +15,18 @@
         <div id="text2" class="hidden-sm hidden-md hidden-lg">
             <h1 style='font-size: 50px;'> Ingredients! </h1>
         </div>
-
     </div>
+
+    
 
     <!--<div class="image">-->
         
     <!--</div>-->
->>>>>>> origin/Project-1
        
+</div>
+    
+<div id="imgHead">
+    Source: <a style=" color: rgba(241, 241, 241, 0.5);" href="http://www.hdnicewallpapers.com/Walls/Big/Nature%20and%20Landscape/Nature_Foggy_Field_Farm_Nice_Wallpapers.jpg">Foggy Farm Wallpaper</a>
 </div>
 </body>
 </html>
--- Project_1/home/home.php
+++ Project_1/home/home.php
@@ -29,11 +29,37 @@ if(isset($_SESSION['username'])){ //checks that it is the same user
         
             <?php include('../navigation.php'); ?>
 
-                <div class="col-md-2"></div>
-                <div id="middle" class="col-md-8"> 
-                    <h3 style="text-align: center">Welcome!</h3>
+                <div id="middle" class="col-md-12 col-sm-12"> 
+                    <h1 style="text-align: center">Welcome!</h1>
                     <br/>
+                    <div class="col-md-2"> </div>
+                    <p class="col-md-8" style="margin-bottom: 15px;">
                     There is unrest in the Galactic Senate. Several thousand solar systems have declared their intentions to leave the Republic. This separatist movement, under the leadership of the mysterious Count Dooku, has made it difficult for the limited number of Jedi Knights to maintain peace and order in the galaxy. Senator Amidala, the former Queen of Naboo, is returning to the Galactic Senate to vote on the critical issue of creating an ARMY OF THE REPUBLIC to assist the overwhelmed Jedi....
+                    </p>
+                    <div class="col-md-2"> </div>
+                    
+
+                    <div class="col-md-4 col-sm-12" id="homeCayenne">
+                        <h3>To Cayenne</h3>
+                        <a href="../ingredients/cayenne.php">
+                            <img src="../ingredients/cayenne.jpg" alt="photo of cayenne" id="HomeImgCayenne" height="200px" width="200px" style="border: 2px solid #193441"/>
+                        </a>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12" id="homeSaffron">
+                        <h3>To Saffron</h3>
+                        <a href="../ingredients/saffron.php">
+                            <img src="../ingredients/saffron.JPG" alt="photo of saffron" id="HomeImgSaffron" height="200px" width="200px" style="border: 2px solid #193441"/>
+                        </a>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12" id="homeSpearmint">
+                        <h3>To Spearmint</h3>
+                         <a href="../ingredients/spearmint.php">
+                            <img src="../ingredients/spearmint.jpg" alt="photo of spearment" id="HomeImgSpearmint" height="200px" width="200px" style="border: 2px solid #193441"/>
+                        </a>
+                    </div>
+
                 </div>
           
              <?php include('../footer.php') ?>
