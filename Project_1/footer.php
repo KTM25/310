a80d97a3e6ef68ec75fca04685dbbfc493a8abf6
KTM25 <ktmangus26@gmail.com>
2017-03-09 23:32:51 -0700

    flipped the contents of footer

--- Project_1/footer.php
+++ Project_1/footer.php
@@ -3,9 +3,9 @@
 
 <div class="footer">
 
-    &copy; Copyright 2017, This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2017sp/index.php">CT 310 </a> Course Project.
+    &copy; Jake Newell and Kevin Mangus
     <br/>
-    Jake Newell and Kevin Mangus 
+    Copyright 2017, This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2017sp/index.php">CT 310 </a> Course Project.
 
 </div>
 
