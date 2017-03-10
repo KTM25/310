2def348af839e702587fff9cd4738faeeb0993c8
KTM25 <ktmangus26@gmail.com>
2017-03-09 23:47:35 -0700

    done header

--- Project_1/header.php
+++ Project_1/header.php
@@ -3,14 +3,6 @@
 <body>
 <div class="jumbotron">
 
-    <img src="../rams.png" class="img-circle hidden-xs" id="Rams" alt="Rams Head" height="126" width="126">
-    
-     <h1 id="page_title" style='font-size: 50px; width: auto;'> Ingredients! </h1>
-
-
-   
-
-
     <div class="hold">
         <img src="../rams.png" class="img-circle hidden-xs" id="Rams" alt="Rams Head" height="126" width="126">
         
@@ -23,12 +15,12 @@
         </div>
     </div>
 
-
 </div>
     
 <div id="imgHead">
     Source: <a style=" color: rgba(241, 241, 241, 0.5);" href="http://www.hdnicewallpapers.com/Walls/Big/Nature%20and%20Landscape/Nature_Foggy_Field_Farm_Nice_Wallpapers.jpg">Foggy Farm Wallpaper</a>
 </div>
+
 </body>
 </html>
 
