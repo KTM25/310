9dd40836389cd5587d232bb7f4ef627b71786914
KTM25 <ktmangus26@gmail.com>
2017-03-09 23:34:53 -0700

    fixed footer again

--- Project_1/footer.php
+++ Project_1/footer.php
@@ -3,9 +3,9 @@
 
 <div class="footer">
 
-    &copy; Jake Newell and Kevin Mangus
+    &copy; Copyright 2017, Jake Newell and Kevin Mangus
     <br/>
-    Copyright 2017, This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2017sp/index.php">CT 310 </a> Course Project.
+    This site is part of a CSU <a href="https://www.cs.colostate.edu/~ct310/yr2017sp/index.php">CT 310 </a> Course Project.
 
 </div>
 
